5ad5073d553787ce3e0c5cf5e79d6fdb9c029d23
dsshen <69359006+dsshen@users.noreply.github.com>
2020-10-11 15:08:04 -0400

    Updates

---
+++ precache-manifest.ffce7f68f484cfabc87ff4236480f6f6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eadcc63053b1c1285e546a7f9d568880",
+    "url": "/Blue-Marble/index.html"
+  },
+  {
+    "revision": "90185addb8bbb97b3c0f",
+    "url": "/Blue-Marble/static/css/2.bdff9e97.chunk.css"
+  },
+  {
+    "revision": "3b4a2d10eb04d8f0030a",
+    "url": "/Blue-Marble/static/css/main.65f42a5e.chunk.css"
+  },
+  {
+    "revision": "90185addb8bbb97b3c0f",
+    "url": "/Blue-Marble/static/js/2.85706a76.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/Blue-Marble/static/js/2.85706a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b4a2d10eb04d8f0030a",
+    "url": "/Blue-Marble/static/js/main.5991e2f4.chunk.js"
+  },
+  {
+    "revision": "b0c4a5a85b7c0970a375",
+    "url": "/Blue-Marble/static/js/runtime-main.0f390038.js"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/Blue-Marble/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "8669b8706bbbdd1482e2fccc4ed96850",
+    "url": "/Blue-Marble/static/media/SourceSansPro-Bold.8669b870.ttf"
+  },
+  {
+    "revision": "c1678b46f7dd3f50ceac94ed4e0ad01a",
+    "url": "/Blue-Marble/static/media/SourceSansPro-Regular.c1678b46.ttf"
+  }
+]);
